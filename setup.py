40f2984ec299aceb3e8ee1748ab01194c1dc5745
rushil-picturehealth <142240474+rushil-picturehealth@users.noreply.github.com>
2025-04-28 17:47:36 -0700

    Update setup.py to resolve import error

--- setup.py
+++ setup.py
@@ -1,4 +1,14 @@
-import setuptools
+from setuptools import setup, find_packages
 
-if __name__ == "__main__":
-    setuptools.setup()
+setup(
+    name="batchgeneratorsv2",
+    version="0.2.3",  # Match what the repo code actually is (optional, but safer)
+    packages=find_packages(),
+    install_requires=[
+        "torch>=2.0.0",
+        "numpy",
+        "fft-conv-pytorch",
+        "batchgenerators>=0.25",
+    ],
+    python_requires=">=3.8",
+)
